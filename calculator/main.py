05bba3562befad0bd888d84b765f3de84b2e2d26
fj7 <85462986+febyjose12@users.noreply.github.com>
2021-11-07 14:47:04 -0500

    Update main.py

--- calculator/main.py
+++ calculator/main.py
@@ -1,16 +1,8 @@
-""" This file makes the calculator """
-
-
-def inc(x_value):
-    """ Increment Function adds one to the x_value"""
-    return x_value + 1
-
-
+""" This is the increment function"""
 class Calculator:
-    """ This is the Calculator class """
+    """ This is the Calculator class"""
 
     result = 0
-
     def get_result(self):
         """ Get Result of Calculation"""
         return self.result
@@ -19,21 +11,11 @@ class Calculator:
         """ adds number to result"""
         self.result = self.result + value_a
         return self.result
-
     def subtract_number(self, value_a):
         """ subtract number from result"""
         self.result = self.result - value_a
         return self.result
-
-    def multiply_number(self, value_a):
-        """ multiply number from result"""
-        self.result = self.result * value_a
+    def multiply_numbers(self, value_a, value_b):
+        """ multiply two numbers and store the result"""
+        self.result = value_a * value_b
         return self.result
-
-    def divide_number(self, value_a):
-        """ divide number from result"""
-        try:
-            self.result = self.result / value_a
-            return self.result
-        except ZeroDivisionError:
-            return ZeroDivisionError
