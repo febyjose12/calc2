f25859c9eba88f96007afad318f3ca0023430bfc
fj7 <85462986+febyjose12@users.noreply.github.com>
2021-12-01 21:00:52 -0500

    Update multiplication.py

--- tests/tests_data/multiplication.py
+++ tests/tests_data/multiplication.py
@@ -1,2 +1,15 @@
 import csv
-x=pd.read_csv('C:\Users\annfe\OneDrive\Desktop\multiplication.csv')
+x=pd.rvalue_1,value_2,result
+2,5,10
+4,4,16
+5,3,15
+5,7,35
+6,8,48
+7,2,14
+8,0,0
+9,0,0
+4,9,36
+5,4,20
+6,2,12
+5,1,5
+ead_csv('C:\Users\annfe\OneDrive\Desktop\multiplication.csv')
