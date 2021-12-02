bd4a06338ca230c4640dcbbaa5811f3b4094afd3
fj7 <85462986+febyjose12@users.noreply.github.com>
2021-12-01 21:00:33 -0500

    Update division.py

--- tests/tests_data/division.py
+++ tests/tests_data/division.py
@@ -1,2 +1,17 @@
 import csv
-x=pd.read_csv('C:\Users\annfe\OneDrive\Desktop\division.csv')
+x=pd.read_csv('C:value_1,value_2,result
+10,2,5
+2,1,2
+4,2,2
+3,2,1.5
+5,2,2.5
+4,2,2
+7,3,2.333333333
+8,4,2
+12,6,2
+12,3,4
+18,9,2
+20,5,4
+21,7,3
+6,2,3
+\Users\annfe\OneDrive\Desktop\division.csv')
